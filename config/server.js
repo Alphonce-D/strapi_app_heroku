34d41ee1c86e86f8f1ce18faf5201268f336df22
Alphonce-D <Teiio@hotmail.fr>
2021-06-23 16:59:40 +0200

    heroku

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4436a3edb2ee0fb4a3701656f766ac41'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4a3ea65a-864c-4494-9ff9-d208965f39e8'
+};
